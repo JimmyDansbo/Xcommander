505f1ec17ea0a376bceea4ff4389a45a82efb142
Jimmy Dansbo <jimmy@dansbo.dk>
2023-07-21 08:01:14 +0200

    Added initial source files to the project

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf *.PRG
+cl65 -t cx16 -o CX16FM.PRG cx16fm.asm
---
+++ cx16fm.asm
@@ -0,0 +1,11 @@
+.segment "STARTUP"
+.segment "INIT"
+.segment "ONCE"
+.segment "CODE"
+
+.include "video.inc"
+.include "file.inc"
+
+main:
+	rts
+	
